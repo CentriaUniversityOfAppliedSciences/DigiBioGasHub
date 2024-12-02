874dab7d9b63ec6e9a7fb08bf6d85d468fc66a0a
Centria-JyriMakela <jyri.makela@centria.fi>
2024-12-02 10:25:09 +0200

    init commit

---
+++ DigiBioGasHub/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DigiBioGasHub',
+  webDir: 'dist'
+};
+
+export default config;
