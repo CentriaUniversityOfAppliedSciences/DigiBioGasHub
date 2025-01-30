b2aa94fc944afed4b39e158712fcba2b75be3beb
Centria-JyriMakela <jyri.makela@centria.fi>
2025-01-30 16:40:52 +0200

    added new fields

--- DigiBioGasHub/src/components/AddOfferComponent.vue
+++ DigiBioGasHub/src/components/AddOfferComponent.vue
@@ -5,48 +5,66 @@
                 <ion-input :label="$t('product.image')" labelPlacement="floating" type="file" @IonChange="processImg"></ion-input>
             </ion-item>
             <ion-item>
-                <ion-select v-model="material" :label="$t('product.material')" :placeholder="$t('product.chooseMaterial')">
+                <ion-select required v-model="material" :label="$t('product.material')" :placeholder="$t('product.chooseMaterial')">
                     <ion-select-option v-for="material in materials" :key="material.name" :value="material.name">{{ material.locality[$i18n.locale] }}</ion-select-option>
                     
                 </ion-select>
             </ion-item>
             <ion-item>
-                <ion-input :label="$t('product.name')" labelPlacement="floating" v-model="productName"></ion-input>
+                <ion-input required :label="$t('product.name')" labelPlacement="floating" v-model="productName"></ion-input>
             </ion-item>
             <ion-item>
-                <ion-select v-model="type" labelPlacement="floating" :label="$t('product.offerTypes')" :placeholder="$t('product.chooseType')">
-                    <ion-select-option value="offer">{{ $t('product.type.offer') }}</ion-select-option>
-                    <ion-select-option value="demand">{{ $t('product.type.demand') }}</ion-select-option>
-                    <ion-select-option value="auction">{{ $t('product.type.auction') }}</ion-select-option>
+                <ion-select required v-model="type" labelPlacement="floating" :label="$t('product.offerTypes')" :placeholder="$t('product.chooseType')">
+                    <ion-select-option value="1">{{ $t('product.type.offer') }}</ion-select-option>
+                    <ion-select-option value="2">{{ $t('product.type.demand') }}</ion-select-option>
+                    <ion-select-option value="3">{{ $t('product.type.auction') }}</ion-select-option>
                 </ion-select>
             </ion-item>
             <ion-item>
                 <ion-textarea :label="$t('product.desc')" labelPlacement="floating" v-model="description"></ion-textarea>
             </ion-item>
             <ion-item v-if="type === 'offer'">
-                <ion-input :label="$t('product.price.offer')" labelPlacement="floating" type="number" v-model="price"></ion-input>
+                <ion-input required :label="$t('product.price.offer')" labelPlacement="floating" type="number" v-model="price"></ion-input>
             </ion-item>
             <ion-item v-if="type === 'demand'">
-                <ion-input :label="$t('product.price.demand')" labelPlacement="floating" type="number" v-model="price"></ion-input>
+                <ion-input required :label="$t('product.price.demand')" labelPlacement="floating" type="number" v-model="price"></ion-input>
             </ion-item>
             <ion-item v-if="type === 'auction'">
-                <ion-input :label="$t('product.price.auction')" labelPlacement="floating" type="number" v-model="price"></ion-input>
+                <ion-input required :label="$t('product.price.auction')" labelPlacement="floating" type="number" v-model="price"></ion-input>
+            </ion-item>
+            <ion-item>
+                <ion-input :label="$t('product.quantity')" labelPlacement="floating" type="number" v-model="quantity"></ion-input>
+            </ion-item>
+            <ion-item>
+                <ion-input :label="$t('product.unit')" labelPlacement="floating" v-model="unit"></ion-input>
             </ion-item>
             <ion-item>
                 <ion-select v-model="logisticType" :label="$t('product.logistic.type')">
-                    <ion-select-option value="included">{{ $t('product.logistic.includedInPrice') }}</ion-select-option>
-                    <ion-select-option value="freetopickup">{{ $t('product.logistic.freeToPickup') }}</ion-select-option>
-                    <ion-select-option value="agreedupon">{{ $t('product.logistic.agreedupon') }}</ion-select-option>
+                    <ion-select-option value="1">{{ $t('product.logistic.includedInPrice') }}</ion-select-option>
+                    <ion-select-option value="2">{{ $t('product.logistic.freeToPickup') }}</ion-select-option>
+                    <ion-select-option value="3">{{ $t('product.logistic.agreedupon') }}</ion-select-option>
                     
+                </ion-select>
+            </ion-item>
+            <ion-item>
+                <ion-datetime :locale="getLocale" :first-day-of-week="1" hour-cycle="h24" >
+                    <span slot="title">{{ $t('product.endTime') }}</span>
+                    <span slot="time-label">{{ $t('product.time') }}</span>
+                </ion-datetime>
+            </ion-item>
+            <ion-item>
+                <ion-select required v-model="visibility">
+                    <ion-select-option value="1">{{ $t('product.visibility.public') }}</ion-select-option>
+                    <ion-select-option value="2">{{ $t('product.visibility.private') }}</ion-select-option>
                 </ion-select>
             </ion-item>
             <ion-button expand="full" @click="addProduct">{{ $t('product.submit') }}</ion-button>
 </template>
 
 <script>
-import {  IonItem, IonLabel, IonInput, IonTextarea, IonButton, IonSelectOption, IonSelect } from '@ionic/vue';
-
-export default {
+import {  IonItem, IonLabel, IonInput, IonTextarea, IonButton, IonSelectOption, IonSelect, IonDatetime } from '@ionic/vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
     name: 'AddOfferComponent',
     components: {
         IonItem,
@@ -56,6 +74,7 @@ export default {
         IonButton,
         IonSelectOption,
         IonSelect,
+        IonDatetime
     },
     data() {
         return {
@@ -64,7 +83,12 @@ export default {
             price: null,
             productImage: null,
             image64: '',
-            type: null
+            type: null,
+            quantity: null,
+            unit: '',
+            logisticType: null,
+            visibility: null,
+
         };
     },
     methods: {
@@ -88,9 +112,19 @@ export default {
             };
             this.image64 = reader.readAsDataURL(file);
             
+        },
+        getLocale(){
+            if(this.$i18n.locale === 'en'){
+                return 'en-GB';
+            } else if(this.$i18n.locale === 'fi'){
+                return 'fi-FI';
+            } else if(this.$i18n.locale === 'sv'){
+                return 'sv-SE';
+            
         }
     }
-};
+}
+});
 </script>
 
 <style scoped>
