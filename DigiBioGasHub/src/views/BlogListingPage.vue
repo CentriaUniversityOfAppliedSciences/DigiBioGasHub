50d356b8c7f126826bafdf7ea7edc5ef38335df4
zprakash <prac303@gmail.com>
2025-02-20 14:17:07 +0200

    udated blog listing

--- DigiBioGasHub/src/views/BlogListingPage.vue
+++ DigiBioGasHub/src/views/BlogListingPage.vue
@@ -1,9 +1,15 @@
 <template>
-    <ion-page>
+       <ion-page>
         <NavBarComponent />
         <ion-content>
-        <BlogListingComponent class="blog-card"/>
-        </ion-content>
+  <ion-grid>
+    <ion-row>
+      <ion-col size="12" size-sm="6" size-md="3" v-for="article in articles" :key="article.title">
+        <BlogListingComponent class="blog-card" :article="article" />
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+</ion-content>
         <FooterComponent />
     </ion-page>
 </template>
@@ -13,17 +19,44 @@ import NavBarComponent from '../components/NavBarComponent.vue'
 import BlogListingComponent from '../components/BlogListingComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
 import {IonPage, IonContent, IonGrid, IonRow, IonCol} from '@ionic/vue'
+import axios from 'axios'
+import slugify from 'slugify'
+
 export default defineComponent ({
     name: 'BlogListingPage',
     components: { NavBarComponent, BlogListingComponent, FooterComponent, IonPage, IonContent, IonGrid, IonRow, IonCol},
-    setup() {
-        return {
-            
-        }
-    },
     data() {
         return {
             articles: []
+        }
+    },
+    mounted() {
+        this.fetchArticles()
+    },
+    methods: {
+        async fetchArticles() {
+            try {
+                var url = "http://localhost:28765/getallblogposts";
+                const response = await axios.post(url, {}, { headers: { 'authorization': localStorage.getItem('token') }, withCredentials: false });
+                console.log(response);
+                if (response.data.result === 'ok' && Array.isArray(response.data.message)) {
+                    this.articles = response.data.message.map(post => ({
+                        title: post.title,
+                        abstract: this.extractAbstract(post.content),
+                        picture: post.image,
+                        link: `/blog/${post.postID}/${slugify(post.title, { lower: true })}`,
+                        date: post.createdAt ? new Date(post.createdAt).toLocaleDateString() : 'Unknown Date'
+                    }));
+                }
+            } catch (error) {
+                console.error('Error fetching articles:', error);
+            }
+        },
+        extractAbstract(htmlContent) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(htmlContent, 'text/html');
+            const firstParagraph = doc.querySelector('p');
+            return firstParagraph ? firstParagraph.textContent.substring(0, 200) + '...' : '';
         }
     }
 })
--- DigiBioGasHub/src/views/BlogPage.vue
+++ DigiBioGasHub/src/views/BlogPage.vue
@@ -8,23 +8,38 @@
     </ion-page>
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import NavBarComponent from '../components/NavBarComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
 import BlogPostComponent from '../components/BlogPostComponent.vue'
-import {IonPage, IonContent} from '@ionic/vue'
-export default defineComponent ({
+import { IonPage, IonContent } from '@ionic/vue'
+import axios from 'axios'
+
+export default defineComponent({
     name: 'ToSPage',
-    components: { NavBarComponent, FooterComponent, IonPage, IonContent, BlogPostComponent},
+    components: { NavBarComponent, FooterComponent, IonPage, IonContent, BlogPostComponent },
     setup() {
-        let content = '';
-        let title = $route.params.title;
-        axios.post('http://localhost:3000/api/blog', {
-            title: title
-        }).then((response) => {
-            content = response.data.content;
-        });
-        return {content
+
+        const content = ref(null)
+        const route = useRoute()
+        const postID = route.params.postID
+
+        async function fetchBlogPost() {
+            try {
+                var url = "http://localhost:28765/getblogpost";
+                const response = await axios.post(url, { "postID": postID }, { headers: { 'authorization': localStorage.getItem('token') }, withCredentials: false });
+                content.value = response.data.message.content;
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        onMounted(() => {
+            fetchBlogPost()
+        })
+
+        return {
+            content
         }
     }
 })
