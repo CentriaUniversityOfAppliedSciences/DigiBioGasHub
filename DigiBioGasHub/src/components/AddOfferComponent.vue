d997fc69a0fdda1936af9e0b722d98b7f41e95c8
zprakash <prac303@gmail.com>
2025-05-26 10:59:08 +0300

    Added form validation and error handling for offer submission

--- DigiBioGasHub/src/components/AddOfferComponent.vue
+++ DigiBioGasHub/src/components/AddOfferComponent.vue
@@ -15,6 +15,7 @@
                 <ion-item>
                     <ion-select required v-model="companyID" :label="$t('menu.company')" :placeholder="$t('product.chooseCompany')">
                         <ion-select-option v-for="comp in companies" :key="comp.name" :value="comp.id">{{ comp.name }}</ion-select-option>
+                        <p v-if="hasError('companyID')" class="error">{{ errors.companyID }}</p>
                     </ion-select>
                 </ion-item>
                 <ion-item>
@@ -24,11 +25,13 @@
                 <ion-item>
                     <ion-select required v-model="material" :label="$t('product.material')" :placeholder="$t('product.chooseMaterial')" @ionChange="materialSelected">
                         <ion-select-option  v-for="mater in materials" :key="mater.name" :value="mater.id">{{ mater.name }}</ion-select-option>
+                        <p v-if="hasError('material')" class="error">{{ errors.material }}</p>
                         
                     </ion-select>
                 </ion-item>
                 <ion-item>
                     <ion-input required :label="$t('product.name')" labelPlacement="floating" v-model="selectedMaterialName" :disabled="true"></ion-input>
+                    <p v-if="hasError('selectedMaterialName')" class="error">{{ errors.selectedMaterialName }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-select required v-model="type" labelPlacement="floating" :label="$t('product.offerTypes')" :placeholder="$t('product.chooseType')">
@@ -39,28 +42,35 @@
                 </ion-item>
                 <ion-item>
                     <ion-textarea :label="$t('product.desc')" labelPlacement="floating" v-model="description"></ion-textarea>
+                    <p v-if="hasError('description')" class="error">{{ errors.description }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-input required v-model="address" :label="$t('general.address')" labelPlacement="floating"></ion-input>
+                    <p v-if="hasError('address')" class="error">{{ errors.address }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-input required v-model="zipcode" :label="$t('general.postalCode')" labelPlacement="floating"></ion-input>
+                    <p v-if="hasError('zipcode')" class="error">{{ errors.zipcode }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-input required v-model="city" :label="$t('general.city')" labelPlacement="floating"> </ion-input>
+                    <p v-if="hasError('city')" class="error">{{ errors.city }}</p>
                 </ion-item>
                 <ion-item >
                     <ion-input required :label="$t('product.price.offer')" labelPlacement="floating" type="number" v-model="price" min="0"></ion-input>
+                    <p v-if="hasError('price')" class="error">{{ errors.price }}</p>
                 </ion-item>
                 
                 <ion-item>
                     <ion-input :label="$t('product.quantity')" labelPlacement="floating" type="number" v-model="quantity" min="0"></ion-input>
+                    <p v-if="hasError('quantity')" class="error">{{ errors.quantity }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-select v-model="unit" :label="$t('product.unit')">
                         <ion-select-option value="1">{{ $t('unit.amount.1') }}</ion-select-option>
                         <ion-select-option value="2">{{ $t('unit.amount.2') }}</ion-select-option>
                     </ion-select>
+                    <p v-if="hasError('unit')" class="error">{{ errors.unit }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-select v-model="logisticType" :label="$t('product.logistic.type')">
@@ -69,24 +79,28 @@
                         <ion-select-option value="3">{{ $t('product.logistic.agreedupon') }}</ion-select-option>
                         
                     </ion-select>
+                    <p v-if="hasError('logisticType')" class="error">{{ errors.logisticType }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-datetime :locale="getLocale()" :first-day-of-week="1" hour-cycle="h23" v-model="startDate" >
                         <span slot="title">{{ $t('product.productDetails.offerValidFrom') }}</span>
                         <span slot="time-label">{{ $t('product.time') }}</span>
                     </ion-datetime>
+                    <p v-if="hasError('startDate')" class="error">{{ errors.startDate }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-datetime :locale="getLocale()" :first-day-of-week="1" hour-cycle="h23" v-model="endDate" >
                         <span slot="title">{{ $t('product.endTime') }}</span>
                         <span slot="time-label">{{ $t('product.time') }}</span>
                     </ion-datetime>
+                    <p v-if="hasError('endDate')" class="error">{{ errors.endDate }}</p>
                 </ion-item>
                 <ion-item>
                     <ion-select required v-model="visibility" :label="$t('product.visibility.info')" :placeholder="$t('product.visibility.chooseVisibility')">
                         <ion-select-option value="1">{{ $t('product.visibility.public') }}</ion-select-option>
                         <ion-select-option value="2">{{ $t('product.visibility.private') }}</ion-select-option>
                     </ion-select>
+                    <p v-if="hasError('visibility')" class="error">{{ errors.visibility }}</p>
                 </ion-item>
                 <ion-button expand="full" @click="addProduct">{{ $t('product.submit') }}</ion-button>
             </ion-card-content>
@@ -153,9 +167,35 @@ export default defineComponent({
             address: '',
             zipcode:'',
             city:'',
+            errors: {}
         };
     },
     methods: {
+
+        validateForm() {
+            this.errors = {};
+
+            if (!this.companyID) this.errors.companyID = this.$t('validation.companyRequired');
+            if (!this.material) this.errors.material = this.$t('validation.materialRequired');
+            if (!this.selectedMaterialName) this.errors.selectedMaterialName = this.$t('validation.materialNameRequired');
+            if (!this.type) this.errors.type = this.$t('validation.typeRequired');
+            if (!this.description) this.errors.description = this.$t('validation.descriptionRequired');
+            if (!this.address) this.errors.address = this.$t('validation.addressRequired');
+            if (!this.zipcode) this.errors.zipcode = this.$t('validation.zipcodeRequired');
+            if (!this.city) this.errors.city = this.$t('validation.cityRequired');
+            if (!this.price || this.price <= 0) this.errors.price = this.$t('validation.priceRequired');
+            if (!this.quantity || this.quantity <= 0) this.errors.quantity = this.$t('validation.quantityRequired');
+            if (!this.unit) this.errors.unit = this.$t('validation.unitRequired');
+            if (!this.logisticType) this.errors.logisticType = this.$t('validation.logisticTypeRequired');
+            if (!this.startDate) this.errors.startDate = this.$t('validation.startDateRequired');
+            if (!this.endDate) this.errors.endDate = this.$t('validation.endDateRequired');
+            if (!this.visibility) this.errors.visibility = this.$t('validation.visibilityRequired');
+
+            return Object.keys(this.errors).length === 0; 
+        },
+        hasError(field) {
+            return this.errors[field];
+        },
         updateTimeZoneStart(event) {
         const date = new Date(event.target.value);
         const timezoneOffset = -date.getTimezoneOffset(); // Get the timezone offset in minutes
@@ -195,6 +235,11 @@ export default defineComponent({
             }
         },
         addProduct() {
+
+            if (!this.validateForm()) {
+                this.$refs.toast.showToast(this.$t('validation.fillAllFields'), 2000, 'danger');
+                return;
+            }
             // Logic to add the product
             try{
             
@@ -253,5 +298,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
-/* Add your styles here */
+
+.error {
+    color: red;
+    font-size: 0.9em;
+    margin-top: 4px;
+}
 </style>
