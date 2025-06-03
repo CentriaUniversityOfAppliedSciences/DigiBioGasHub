e40fdcf041d56209697719fbe9f20868e91bc37d
zprakash <prac303@gmail.com>
2025-06-03 15:28:17 +0300

    Add email validation and error message for invitation in CompanyComponent

--- DigiBioGasHub/src/components/CompanyComponent.vue
+++ DigiBioGasHub/src/components/CompanyComponent.vue
@@ -265,8 +265,10 @@ export default defineComponent({
             this.inviteEmail = '';
         },
         async sendInvite() {
-            if (!this.inviteEmail) {
-                alert(this.$t('general.enterValidEmail'));
+            const emailPattern = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+
+            if (!this.inviteEmail || !emailPattern.test(this.inviteEmail)) {
+                this.$refs.toastComponent.showToast(this.$t('invitations.invalidEmail'), 2000, 'danger');
                 return;
             }
 
