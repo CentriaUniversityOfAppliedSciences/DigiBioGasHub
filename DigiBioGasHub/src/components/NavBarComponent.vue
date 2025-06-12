f7596719134967abb5a584e33e37935a92ca84d0
zprakash <prac303@gmail.com>
2025-06-12 13:47:01 +0300

    Add hamburger menu to navbar for mobile and Clean up

--- DigiBioGasHub/src/components/NavBarComponent.vue
+++ DigiBioGasHub/src/components/NavBarComponent.vue
@@ -1,72 +1,100 @@
 <template>
-    <nav class="navbar">
-        <div class="navbar-left">
-            <img :src="$t('menu.logoPath')" alt="Funder Logo" class="logo">
-            <button class="feedback-button"
-                onclick="window.open('https://link.webropolsurveys.com/S/941E350F901601ED', '_blank')">
-                <span>{{$t('menu.giveFeedback')}}</span>
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="white" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </svg>
-            </button>
-        </div>
-        <div class="navbar-center">
-            <img src="@/assets/DBH-logo.png" alt="DigiBioGasHubs Logo" class="logo">
-        </div>
-        <div class="navbar-right">
+    <ion-header>
+        <ion-toolbar>
+            <nav class="navbar">
+                <div class="navbar-left">
+                    <img :src="$t('menu.logoPath')" alt="Funder Logo" class="logo">
 
-            <ion-buttons slot="end">
-                <NotificationComponent />
-            </ion-buttons>
-            <ul class="menu">
-                <li class="menu-item"><a href="/home">{{$t('menu.home')}}</a></li>
+                    <button class="feedback-button" v-if="!isMobile" @click="openFeedback">
+                        <span>{{ $t('menu.giveFeedback') }}</span>
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="white" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="navbar-center">
+                    <img src="@/assets/DBH-logo.png" alt="DigiBioGasHubs Logo" class="logo">
+                </div>
+
+                <div class="navbar-right">
+                    <ion-buttons slot="end">
+                        <NotificationComponent />
+                        <button class="hamburger" @click="toggleMenu" v-if="isMobile">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3 6H21M3 12H21M3 18H21" stroke="white" stroke-width="2"
+                                    stroke-linecap="round" />
+                            </svg>
+                        </button>
+                    </ion-buttons>
+                </div>
+            </nav>
+
+            <ul class="menu" :class="{ mobile: isMobile, open: showMenu }" v-if="!isMobile || showMenu">
+                <li class="menu-item"><a href="/home">{{ $t('menu.home') }}</a></li>
                 <li class="menu-item"><a href="/marketplace">{{ $t('menu.marketplace') }}</a></li>
                 <li class="menu-item"><a href="/map">{{ $t('menu.map') }}</a></li>
                 <li class="menu-item"><a href="/articles">{{ $t('menu.articles') }}</a></li>
                 <li class="menu-item"><a href="/chat">{{ $t('menu.chat') }}</a></li>
-                <!-- <li class="menu-item"><a href="/knowledge-base">{{ $t('menu.knowledgeBase') }}</a></li> -->
-                <li v-if="!LoggedIn" class="menu-item" id="loginbtn" ><a href="/login">{{ $t('menu.login') }}</a></li>
+                <li v-if="!LoggedIn" class="menu-item" id="loginbtn"><a href="/login">{{ $t('menu.login') }}</a></li>
                 <li v-if="LoggedIn" class="menu-item"><a href="/contracts">{{ $t('menu.mycontracts') }}</a></li>
-                <li v-if="LoggedIn" class="menu-item" ><a href="/company">{{ $t('menu.mycompany') }}</a></li>
+                <li v-if="LoggedIn" class="menu-item"><a href="/company">{{ $t('menu.mycompany') }}</a></li>
                 <li v-if="LoggedIn" class="menu-item"><a href="/profile">{{ $t('menu.profile') }}</a></li>
-                <li v-if="LoggedIn" class="menu-item"><LogoutComponent /></li>
-                <li v-if="Admin" class="menu-item"><AdminComponent /></li>
-                <li class="menu-item"><LocaleComponent /></li>
+                <li v-if="LoggedIn" class="menu-item">
+                    <LogoutComponent />
+                </li>
+                <li v-if="Admin" class="menu-item">
+                    <AdminComponent />
+                </li>
+                <li class="menu-item">
+                    <LocaleComponent />
+                </li>
+                <li v-if="isMobile" class="menu-item">
+                    <button class="feedback-button" @click="openFeedback">
+                        <span>{{ $t('menu.giveFeedback') }}</span>
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="white" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </button>
+                </li>
             </ul>
-        </div>
-    </nav>
+
+        </ion-toolbar>
+    </ion-header>
 </template>
 
 <script>
-import {defineComponent} from 'vue';
+import { defineComponent } from 'vue';
 import AdminComponent from './AdminComponent.vue';
 import LocaleComponent from './LocaleComponent.vue';
 import LoginComponent from './LoginComponent.vue';
 import LogoutComponent from './LogoutComponent.vue';
 import NotificationComponent from './NotificationComponent.vue';
 import { IonButtons } from '@ionic/vue';
-export default defineComponent ({
+export default defineComponent({
     name: 'NavBarComponent',
     components: { LocaleComponent, LoginComponent, AdminComponent, LogoutComponent, NotificationComponent, IonButtons },
     setup() {
         return {};
     },
     methods: {
-        handleLoginModal(){
+        handleLoginModal() {
             this.showModal = true;
         },
-        closeModal(){
+        closeModal() {
             this.showModal = false;
         },
-        decodeJWT(token){
+        decodeJWT(token) {
             try {
                 return JSON.parse(atob(token.split('.')[1]));
             } catch (e) {
                 return null;
             }
         },
-        checkCompany(){
+        checkCompany() {
             let token = localStorage.getItem('token');
             if (token) {
                 let decoded = this.decodeJWT(token);
@@ -77,7 +105,7 @@ export default defineComponent ({
                 }
             }
         },
-        checkLogin(){
+        checkLogin() {
             let token = localStorage.getItem('token');
             if (token) {
                 let decoded = this.decodeJWT(token);
@@ -88,7 +116,7 @@ export default defineComponent ({
                 }
             }
         },
-        checkAdmin(){
+        checkAdmin() {
             let token = localStorage.getItem('token');
             if (token) {
                 let decoded = this.decodeJWT(token);
@@ -98,6 +126,15 @@ export default defineComponent ({
                     }
                 }
             }
+        },
+        toggleMenu() {
+            this.showMenu = !this.showMenu;
+        },
+        checkMobile() {
+            this.isMobile = window.innerWidth <= 768;
+        },
+        openFeedback() {
+            window.open('https://link.webropolsurveys.com/S/941E350F901601ED', '_blank');
         }
     },
     data() {
@@ -105,13 +142,20 @@ export default defineComponent ({
             showModal: false,
             LoggedIn: false,
             inCompany: false,
-            Admin: false
+            Admin: false,
+            isMobile: false,
+            showMenu: false
         }
     },
     mounted() {
         this.checkLogin();
         this.checkCompany();
         this.checkAdmin();
+        this.checkMobile();
+        window.addEventListener('resize', this.checkMobile);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.checkMobile);
     }
 });
 </script>
@@ -127,7 +171,9 @@ export default defineComponent ({
     z-index: 1000;
 }
 
-.navbar-left, .navbar-center, .navbar-right {
+.navbar-left,
+.navbar-center,
+.navbar-right {
     flex: 1;
     display: flex;
     justify-content: center;
@@ -147,15 +193,15 @@ export default defineComponent ({
     gap: 8px;
     margin-left: 10px;
     transition: all 0.3s ease;
-  }
+}
 
-  .feedback-button span {
+.feedback-button span {
     transition: text-decoration 0.3s ease;
-  }
+}
 
-  .feedback-button:hover span {
+.feedback-button:hover span {
     text-decoration: underline;
-  }
+}
 
 .navbar-left {
     justify-content: flex-start;
@@ -173,16 +219,26 @@ export default defineComponent ({
     list-style: none;
     display: flex;
     gap: 20px;
-    
+    padding: 0;
+    margin: 0;
+    flex-wrap: wrap;
 }
-.menu-item{
+
+.menu.mobile {
+    flex-direction: column;
+    background-color: #333;
+    width: 100%;
+    padding: 10px 0;
+}
+
+.menu-item {
     margin: auto;
 }
 
 .menu-item a {
     color: #fff;
     text-decoration: none;
-    
+
 }
 
 .menu-item a:hover {
@@ -195,5 +251,29 @@ export default defineComponent ({
     padding: 10px 20px;
     border-radius: 5px;
     transition: background-color 0.3s ease;
+}
+
+.menu.mobile .menu-item {
+    text-align: center;
+}
+
+.hamburger {
+    background: none;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+}
+
+@media (max-width: 768px) {
+    .menu:not(.mobile):not(.open) {
+        display: none !important;
+    }
+
+    .menu.mobile .feedback-button {
+        width: 100%;
+        justify-content: center;
+    }
 }
 </style>
