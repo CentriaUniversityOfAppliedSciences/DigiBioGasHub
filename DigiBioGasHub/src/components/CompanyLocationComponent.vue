7d5b9f16b4d1cc5caf667ed339723d98110d62e5
zprakash <prac303@gmail.com>
2025-06-26 14:23:11 +0300

    Add edit location

--- DigiBioGasHub/src/components/CompanyLocationComponent.vue
+++ DigiBioGasHub/src/components/CompanyLocationComponent.vue
@@ -1,36 +1,51 @@
 <template>
     <ion-card class="location-card">
-        <ion-card-header>
-            <ion-item>
-                <ion-card-title class="center-icon"><ion-icon name="location"></ion-icon></ion-card-title>
-            </ion-item>
-        </ion-card-header>
-        <ion-card-content>
-            <ion-list>
-                <ion-item>
-                    <ion-label style="margin-right: 12px;">{{ $t('general.company') }}:</ion-label>
-                    <ion-input v-model="location.name" type="text" readonly></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-label style="margin-right: 12px;">{{ $t('general.address') }}:</ion-label>
-                    <ion-input v-model="location.address" type="text" readonly></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-label style="margin-right: 12px;">{{ $t('general.city') }}:</ion-label>
-                    <ion-input v-model="location.city" type="text" readonly></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-label style="margin-right: 12px;">{{ $t('general.postalCode') }}:</ion-label>
-                    <ion-input v-model="location.zipcode" type="text" readonly></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-button v-if="companyData.userlevel === '23' || companyData.userlevel === '99'"
-                        @click="confirmDelete()" id="deleteCompany" color="danger">{{ $t('menu.delete') }}</ion-button>
-                </ion-item>
-            </ion-list>
-        </ion-card-content>
+      <ion-card-header>
+        <ion-item>
+          <ion-card-title class="center-icon">
+            <ion-icon name="location"></ion-icon>
+          </ion-card-title>
+        </ion-item>
+      </ion-card-header>
+  
+      <ion-card-content>
+        <ion-list>
+          <ion-item>
+            <ion-label style="margin-right: 12px;">{{ $t('general.company') }}:</ion-label>
+            <ion-input :value="location.name" type="text" readonly></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label style="margin-right: 12px;">{{ $t('general.address') }}:</ion-label>
+            <ion-input v-model="editLocation.address" :readonly="!showEdit" type="text"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label style="margin-right: 12px;">{{ $t('general.city') }}:</ion-label>
+            <ion-input v-model="editLocation.city" :readonly="!showEdit" type="text"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label style="margin-right: 12px;">{{ $t('general.postalCode') }}:</ion-label>
+            <ion-input v-model="editLocation.zipcode" :readonly="!showEdit" type="text"></ion-input>
+          </ion-item>
+          <ion-item>
+            <template v-if="showEdit">
+              <ion-button @click="saveLocation" color="success">{{ $t('general.save') }}</ion-button>
+              <ion-button @click="cancelEdit" color="medium">{{ $t('general.cancel') }}</ion-button>
+            </template>
+            <template v-else>
+              <ion-button v-if="companyData.userlevel === '23'" @click="enableEdit" color="primary">
+                {{ $t('menu.edit') }}
+              </ion-button>
+              <ion-button v-if="companyData.userlevel === '23' || companyData.userlevel === '99'"
+                @click="confirmDelete" id="deleteCompany" color="danger">
+                {{ $t('menu.delete') }}
+              </ion-button>
+            </template>
+          </ion-item>
+        </ion-list>
+      </ion-card-content>
     </ion-card>
-</template>
+  </template>
+  
 
 <script>
 import { IonPage, IonContent, IonToolbar, IonTitle, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList, alertController, IonIcon } from '@ionic/vue';
@@ -85,6 +100,12 @@ export default defineComponent({
     emits: ['locationDeleted'],
     data() {
         return {
+            showEdit: false,
+            editLocation: {
+                address: this.location.address,
+                city: this.location.city,
+                zipcode: this.location.zipcode
+            },
             alertButtons: [
                 {
                     text: this.$t('general.cancel'),
@@ -102,6 +123,41 @@ export default defineComponent({
         };
     },
     methods: {
+        enableEdit() {
+            this.editLocation = {
+                address: this.location.address,
+                city: this.location.city,
+                zipcode: this.location.zipcode,
+            };
+            this.showEdit = true;
+        },
+        cancelEdit() {
+            this.showEdit = false;
+            this.editLocation = {
+                address: this.location.address,
+                city: this.location.city,
+                zipcode: this.location.zipcode,
+            };
+        },
+        saveLocation() {
+            const url = this.$api_add + "/company/updatelocation";
+            axios.post(url, {
+                locationID: this.location.id,
+                companyID: this.location.companyID,
+                address: this.editLocation.address,
+                city: this.editLocation.city,
+                zipcode: this.editLocation.zipcode,
+            }, {
+                headers: { 'authorization': localStorage.getItem('token') },
+            }).then((response) => {
+                if (response.data.type === "result" && response.data.result === "ok") {
+                    this.location.address = this.editLocation.address;
+                    this.location.city = this.editLocation.city;
+                    this.location.zipcode = this.editLocation.zipcode;
+                    this.showEdit = false;
+                }
+            });
+        },
         confirmDelete() {
             this.alertController
                 .create({
