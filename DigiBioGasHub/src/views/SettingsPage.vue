71dd36087ac27724bacce5528bd6d23886e369f5
zprakash <prac303@gmail.com>
2025-07-01 11:28:51 +0300

    Update profile settings ui

--- DigiBioGasHub/src/views/SettingsPage.vue
+++ DigiBioGasHub/src/views/SettingsPage.vue
@@ -1,121 +1,134 @@
 <template>
-    <ion-page>
-        <ion-content>
-            <NavBarComponent />
-            <ion-grid>
-                <ion-row>
-                    <ion-col size="12">
-                        <ion-label>{{ $t("settings.filter") }}</ion-label>
-                    </ion-col>
-                </ion-row>  
-                <ion-row>
-                    <ion-col size="3">
-                        <ion-checkbox v-model="settings.filter.feedstock">{{ $t("settings.feedstock") }}</ion-checkbox>
-                    </ion-col>                        
-                    <ion-col size="3">
-                        <ion-checkbox v-model="settings.filter.biogas">{{ $t("settings.biogas") }}</ion-checkbox>
-                    </ion-col>
-                    <ion-col size="3">
-                        <ion-checkbox v-model="settings.filter.logistics">{{ $t("settings.logistics") }}</ion-checkbox>
-                    </ion-col>
-                    <ion-col size="3">
-                        <ion-checkbox v-model="settings.filter.digestate">{{ $t("settings.digestate") }}</ion-checkbox>
-                    </ion-col>
-                </ion-row>
-                <ion-row>
-                    <ion-col size="12">
-                        <ion-label>{{ $t("settings.chat") }}</ion-label>
-                    </ion-col>
-                
-                </ion-row>
-                <ion-row>
-                    <ion-col size="3">
-                        <ion-checkbox v-model="settings.chat.chatVisibility">{{ $t("settings.chatVisibility") }}</ion-checkbox>
-                    </ion-col>                        
-                    
-                </ion-row>
-                <ion-row>
-                    <ion-col size="12">
-                        <ion-button @click="saveSettings">{{ $t("menu.save") }}</ion-button>
-                    </ion-col>
-                </ion-row>
-                
-                    
-                
-            </ion-grid>
-        </ion-content>
-        <FooterComponent />
-    </ion-page>
+  <ion-page>
+    <ion-content>
+      <NavBarComponent />
+
+      <ion-grid style="max-width: 90rem;">
+        <ion-row>
+          <ion-col size="12">
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>{{ $t("settings.filter") }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <ion-list>
+                  <ion-item>
+                    <ion-label>{{ $t("settings.feedstock") }}</ion-label>
+                    <ion-checkbox slot="end" v-model="settings.filter.feedstock" />
+                  </ion-item>
+                  <ion-item>
+                    <ion-label>{{ $t("settings.biogas") }}</ion-label>
+                    <ion-checkbox slot="end" v-model="settings.filter.biogas" />
+                  </ion-item>
+                  <ion-item>
+                    <ion-label>{{ $t("settings.logistics") }}</ion-label>
+                    <ion-checkbox slot="end" v-model="settings.filter.logistics" />
+                  </ion-item>
+                  <ion-item>
+                    <ion-label>{{ $t("settings.digestate") }}</ion-label>
+                    <ion-checkbox slot="end" v-model="settings.filter.digestate" />
+                  </ion-item>
+                </ion-list>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+
+        <ion-row>
+          <ion-col size="12">
+            <ion-card>
+              <ion-card-header>
+                <ion-card-title>{{ $t("settings.chat") }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <ion-list>
+                  <ion-item>
+                    <ion-label>{{ $t("settings.chatVisibility") }}</ion-label>
+                    <ion-checkbox slot="end" v-model="settings.chat.chatVisibility" />
+                  </ion-item>
+                </ion-list>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="ion-padding-top">
+          <ion-col size="12" class="ion-text-center">
+            <ion-button @click="saveSettings" expand="block">
+              {{ $t("menu.save") }}
+            </ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <FooterComponent />
+    </ion-content>
+  </ion-page>
 </template>
+
 
 <script>
 import { defineComponent } from 'vue'
-import { IonPage, IonContent, IonList, IonItem, IonLabel, IonCheckbox, IonGrid, IonRow, IonCol, IonButton } from '@ionic/vue'
+import { IonPage, IonContent, IonList, IonItem, IonLabel, IonCheckbox, IonGrid, IonRow, IonCol, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/vue'
 import NavBarComponent from '../components/NavBarComponent.vue';
 import FooterComponent from '../components/FooterComponent.vue';
 import axios from 'axios';
 
 export default defineComponent({
-    name: 'SettingsPage',
-    components: { IonPage, IonContent, NavBarComponent, FooterComponent, IonList, IonItem, IonLabel, IonCheckbox, IonGrid, IonRow, IonCol, IonButton },
-    data() {
-        return {
-            settings: {
-                filter:{
-                    feedstock: false,
-                    biogas: false,
-                    logistics: false,
-                    digestate: false,
-                },
-                chat:{
-                    chatVisibility: false
-                }
-            }
-        }
-    },
-    mounted() {
-        this.getSettings();
-    },
-    methods: {
-        getSettings() {
-            var url = this.$api_add + "/getsettings";
-            axios.post(url,{}, {headers:{ 'authorization':localStorage.getItem('token') }, withCredentials: false}).then((response) => {
-                if (response.data.result == "ok" && response.data.message != null && response.data.message != undefined){
-                    var data = response.data.message;
-
-                    if (data.length > 0) {
-                        for (let i = 0; i < data.length; i++) {
-                            var key = data[i].key;
-                            var value = data[i].value;
-                            this.settings[key] = value;
-                        }
-                    }
-                }
-            });
+  name: 'SettingsPage',
+  components: { IonPage, IonContent, NavBarComponent, FooterComponent, IonList, IonItem, IonLabel, IonCheckbox, IonGrid, IonRow, IonCol, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent },
+  data() {
+    return {
+      settings: {
+        filter: {
+          feedstock: false,
+          biogas: false,
+          logistics: false,
+          digestate: false,
         },
-        saveSettings() {
-
-            
-            var url = this.$api_add + "/updatesettings";
-
-            axios.post(url,{"settings":this.settings},{headers:{ 'authorization':localStorage.getItem('token') }, withCredentials: false}).then((response) => {
-                if (response.data.result == "ok" && response.data.message != null && response.data.message != undefined){
-
-                    var data = response.data.message;
-                    if (data.length > 0) {
-                        for (let i = 0; i < data.length; i++) {
-                            var key = data[i].key;
-                            var value = data[i].value;
-                            this.settings[key] = value;
-                        }
-                    }
-                }
-            });
+        chat: {
+          chatVisibility: false
         }
+      }
     }
+  },
+  mounted() {
+    this.getSettings();
+  },
+  methods: {
+    getSettings() {
+      var url = this.$api_add + "/getsettings";
+      axios.post(url, {}, { headers: { 'authorization': localStorage.getItem('token') }, withCredentials: false }).then((response) => {
+        if (response.data.result == "ok" && response.data.message != null && response.data.message != undefined) {
+          var data = response.data.message;
 
+          if (data.length > 0) {
+            for (let i = 0; i < data.length; i++) {
+              var key = data[i].key;
+              var value = data[i].value;
+              this.settings[key] = value;
+            }
+          }
+        }
+      });
+    },
+    saveSettings() {
+      var url = this.$api_add + "/updatesettings";
+      axios.post(url, { "settings": this.settings }, { headers: { 'authorization': localStorage.getItem('token') }, withCredentials: false }).then((response) => {
+        if (response.data.result == "ok" && response.data.message != null && response.data.message != undefined) {
+
+          var data = response.data.message;
+          if (data.length > 0) {
+            for (let i = 0; i < data.length; i++) {
+              var key = data[i].key;
+              var value = data[i].value;
+              this.settings[key] = value;
+            }
+          }
+        }
+      });
+    }
+  }
 });
-
-
 
 </script>
