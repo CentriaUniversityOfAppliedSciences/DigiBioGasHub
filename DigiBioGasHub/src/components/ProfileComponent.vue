7e7f52a0535c07963a3171f3d825113a319317fe
zprakash <prac303@gmail.com>
2025-07-07 14:33:45 +0300

    Update localisation for API key button

--- DigiBioGasHub/src/components/ProfileComponent.vue
+++ DigiBioGasHub/src/components/ProfileComponent.vue
@@ -22,7 +22,7 @@
                     <ion-button id="deleteUser" color="danger">{{ $t('menu.deleteAccount') }}</ion-button>
                     <ion-button id="changePassword">{{ $t('menu.changePassword') }}</ion-button>
                     <ion-button @click="settings_button">{{ $t("general.settings") }}</ion-button>
-                    <ion-button @click="goToApiKey">API KEYS</ion-button>
+                    <ion-button @click="goToApiKey">{{ $t("apiKeys.title") }}</ion-button>
                 </ion-card-content>
             </ion-card>
             
