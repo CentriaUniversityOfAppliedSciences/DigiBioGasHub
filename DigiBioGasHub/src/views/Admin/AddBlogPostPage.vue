b5802cd14e855884e6a2c45f6d35c8b04e606280
zprakash <prac303@gmail.com>
2025-10-17 14:50:03 +0300

    Remove FooterComponent from AddBlogPostPage

--- DigiBioGasHub/src/views/Admin/AddBlogPostPage.vue
+++ DigiBioGasHub/src/views/Admin/AddBlogPostPage.vue
@@ -3,15 +3,12 @@
         <NavBarComponent />
         <ion-content :fullscreen="true">
             <AddBlogPostComponent />
-            
         </ion-content>
-        <FooterComponent />
     </IonPage>
 </template>
 <script>
 import { IonPage, IonContent } from '@ionic/vue'
 import NavBarComponent from '../../components/NavBarComponent.vue';
-import FooterComponent from '../../components/FooterComponent.vue';
 import AddBlogPostComponent from '../../components/AddBlogPostComponent.vue';
 import { defineComponent } from 'vue';
 export default defineComponent({
@@ -20,13 +17,7 @@ export default defineComponent({
         IonPage,
         IonContent,
         NavBarComponent,
-        FooterComponent,
         AddBlogPostComponent
-    },
-    data() {
-        return {
-            
-        }
     }
 })
 </script>
