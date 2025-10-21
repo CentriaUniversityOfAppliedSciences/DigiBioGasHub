31092e4dab37da69df26ba5ca61b05aea7209fc3
zprakash <prac303@gmail.com>
2025-10-21 14:47:56 +0300

    Update company location registration UI

--- DigiBioGasHub/src/components/CompanyLocationRegisterComponent.vue
+++ DigiBioGasHub/src/components/CompanyLocationRegisterComponent.vue
@@ -9,30 +9,40 @@
                     <ion-title style="text-align: center;">{{ $t('company.locationRegistration.title') }}</ion-title>
                 </ion-toolbar>
                 <ion-row>
+                    <p class="required-note">
+                        {{ $t('validation.requiredField') }}
+                    </p>
+
                     <ion-col size="12">
                         <ion-item>
-                            <ion-label position="floating">{{ $t('general.company') }}</ion-label>
+                            <ion-label position="stacked" class="label">{{ $t('general.company') }} <span
+                                    class="required-asterisk">*</span></ion-label>
                             <ion-input v-model="form.companyName" type="text" readonly></ion-input>
                         </ion-item>
                     </ion-col>
                     <ion-col size="12">
                         <ion-item>
-                            <ion-label position="floating">{{ $t('general.address') }}</ion-label>
-                            <ion-input v-model="form.address" type="text"></ion-input>
+                            <ion-label position="stacked" class="label">{{ $t('general.address') }}<span
+                                    class="required-asterisk">*</span></ion-label>
+                            <ion-input v-model="form.address" type="text"
+                                :placeholder="$t('company.address')"></ion-input>
                             <p v-if="hasError('address')" class="error">{{ errors.address }}</p>
                         </ion-item>
                     </ion-col>
                     <ion-col size="12">
                         <ion-item>
-                            <ion-label position="floating">{{ $t('general.city') }}</ion-label>
-                            <ion-input v-model="form.city" type="text"></ion-input>
+                            <ion-label position="stacked" class="label">{{ $t('general.city') }}<span
+                                    class="required-asterisk">*</span></ion-label>
+                            <ion-input v-model="form.city" type="text" :placeholder="$t('company.city')"></ion-input>
                             <p v-if="hasError('city')" class="error">{{ errors.city }}</p>
                         </ion-item>
                     </ion-col>
                     <ion-col size="12">
                         <ion-item>
-                            <ion-label position="floating">{{ $t('general.postalCode') }}</ion-label>
-                            <ion-input v-model="form.zipcode" type="text"></ion-input>
+                            <ion-label position="stacked" class="label">{{ $t('general.postalCode') }}<span
+                                    class="required-asterisk">*</span></ion-label>
+                            <ion-input v-model="form.zipcode" type="text"
+                                :placeholder="$t('company.postalCode')"></ion-input>
                             <p v-if="hasError('zipcode')" class="error">{{ errors.zipcode }}</p>
                         </ion-item>
                     </ion-col>
@@ -45,9 +55,9 @@
             </ion-grid>
 
             <ToastComponent ref="toastComponent" />
-
+            <FooterComponent />
         </ion-content>
-        <FooterComponent />
+
     </ion-page>
 </template>
 
@@ -101,9 +111,9 @@ export default defineComponent({
 
             if (!this.form.address || this.form.address.trim() == '') this.errors.address = this.$t('validation.addressRequired');
             if (!this.form.zipcode || this.form.zipcode.trim() == '') this.errors.zipcode = this.$t('validation.zipcodeRequired');
-            if (!this.form.city || this.form.city.trim() == '' ) this.errors.city = this.$t('validation.cityRequired');
+            if (!this.form.city || this.form.city.trim() == '') this.errors.city = this.$t('validation.cityRequired');
 
-            return Object.keys(this.errors).length === 0; 
+            return Object.keys(this.errors).length === 0;
         },
         hasError(field) {
             return this.errors[field];
@@ -155,6 +165,19 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.required-note {
+    padding: 0.5rem;
+    border-radius: 10px;
+    color: var(--color);
+    background-color: transparent;
+    text-align: center;
+    border: 2px solid var(--ion-color-warning);
+    max-width: 18rem;
+    margin: auto;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+}
+
 .form-grid {
     max-width: 700px;
     width: 100%;
@@ -163,6 +186,17 @@ export default defineComponent({
     border-radius: 8px;
     border: 2px solid gray;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    margin-bottom: 1rem;
+}
+
+.label {
+    font-size: 1.5rem;
+    margin-bottom: 5px;
+}
+
+.required-asterisk {
+    color: red;
+    font-size: 20PX;
 }
 
 .error {
